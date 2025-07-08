61abbbeb1b16820a1c1801ea870726160d218ade
SheldonChang <sheldon.chang@jet-opto.com.tw>
2025-07-08 16:49:40 +0800

    Init project
    Change-Id: I726f7bb72c0cd7c42915958e0cd5bad2664e2029

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <nav className="bg-gray-100 shadow-lg fixed top-0 left-0 right-0 z-50">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between h-16">
+                    <div className="flex-shrink-0">
+                        <a href="#" className="text-white font-bold text-xl">MyLogo</a>
+                    </div>
+                    <div className="hidden md:block">
+                        <div className="ml-10 flex items-baseline space-x-4">
+                            <a href="#" className="bg-gray-200 text-white px-3 py-2 rounded-md text-sm font-medium">關於我</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">經歷</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">作品</a>
+                        </div>
+                    </div>
+                    <div className="-mr-2 flex md:hidden">
+                        <button
+                            onClick={() => setIsOpen(!isOpen)}
+                            type="button"
+                            className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none"
+                            aria-controls="mobile-menu"
+                            aria-expanded={isOpen} >
+                            <span className="sr-only">Open main menu</span>
+                            <svg className={`block h-6 w-6 ${isOpen ? 'hidden' : 'block'}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+                            <svg className={`h-6 w-6 ${isOpen ? 'block' : 'hidden'}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            {isOpen && (
+                <div className="md:hidden" id="mobile-menu">
+                    <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                        <a href="#" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">關於我</a>
+                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">經歷</a>
+                        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">作品</a>
+                    </div>
+                </div>
+            )}
+        </nav>
+    );
+};
+
+export default Navbar;
